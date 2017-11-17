dc264338b905ecb5c1b5235a316af8b717486050
Chris Janes <gingoreilly@gmail.com>
2017-11-17 13:22:30 +0000

    initial commit of expensify and react boilerplate

---
+++ boilerplate-v1/webpack.config.js
@@ -0,0 +1,29 @@
+// entry point - src/app.js => output - public/scripts/app.js
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      loader: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};
