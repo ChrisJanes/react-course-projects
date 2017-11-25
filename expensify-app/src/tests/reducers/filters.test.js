7c9fbc2cec7b1d012102bda3fd55ff36e0fa1676
Chris Janes <gingoreilly@gmail.com>
2017-11-25 15:20:08 +0000

    Add tests for filter reducers and expenses.

--- expensify-app/src/tests/reducers/filters.test.js
+++ expensify-app/src/tests/reducers/filters.test.js
@@ -17,6 +17,38 @@ test('should set sortBy to amount', () => {
 });
 
 test('should set sortBy to date', () => {
-  const state = filtersReducer(undefined, {type:'SORT_BY_DATE'});
+  const currentState = {
+    text: '',
+    startDate: undefined,
+    endDate: undefined,
+    sortBy: 'amount'
+  }
+  const action = {type:'SORT_BY_DATE'};
+  const state = filtersReducer(currentState, action);
   expect(state.sortBy).toBe('date');
 });
+
+test('should set text filter', () => {
+  const state = filtersReducer(undefined, {type:'SET_TEXT_FILTER', text: 'rent'});
+  expect(state.text).toBe('rent');
+});
+
+test('should set startDate filter', () => {
+  const startDate = moment();
+  const action = {
+    type:'SET_START_DATE',
+    startDate
+  }
+  const state= filtersReducer(undefined, action);
+  expect(state.startDate).toBe(startDate);
+});
+
+test('should set endDate filter', () => {
+  const endDate = moment();
+  const action = {
+    type:'SET_END_DATE',
+    endDate
+  }
+  const state= filtersReducer(undefined, action);
+  expect(state.endDate).toBe(endDate);
+})
