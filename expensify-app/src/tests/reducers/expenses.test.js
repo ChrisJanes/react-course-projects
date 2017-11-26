869a6262a265ca5083588fbe813b0fc517c9cd01
Chris Janes <gingoreilly@gmail.com>
2017-11-26 15:22:57 +0000

    add more tests for expense reducers

--- expensify-app/src/tests/reducers/expenses.test.js
+++ expensify-app/src/tests/reducers/expenses.test.js
@@ -1,5 +1,8 @@
+import moment from 'moment';
+
 import expensesReducer from '../../reducers/expenses';
 import expenses from '../fixtures/expenses';
+
 
 test('should set default state', () => {
   const state = expensesReducer(undefined, {type: '@@INIT'});
@@ -11,6 +14,53 @@ test('should remove expense by id', () => {
     type: 'REMOVE_EXPENSE',
     id: expenses[1].id
   };
-  const state = expensesReducer(undefined, action);
+  const state = expensesReducer(expenses, action);
   expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expenses if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '-5'
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
+
+test('should add an expense', () => {
+  const expense = {
+    id: '4',
+    description: 'Test',
+    note: '',
+    amount: 250,
+    createdAt: moment(0).add(6, 'days').valueOf()
+  }
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense
+  }
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([...expenses, expense]);
+});
+
+test('should edit an expense', () => {
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: expenses[1].id,
+    updates: {
+      amount: 5000
+    }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state[1].amount).toEqual(5000);
+});
+
+test('should not edit expense if expense not found', () => {
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: '-1',
+    updates: {description: 'testing'}
+  }
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
 });
