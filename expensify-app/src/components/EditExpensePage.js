7a100db0f4f27b8f54269e01f2eeca8a1ddbfba8
Chris Janes <gingoreilly@gmail.com>
2017-12-13 15:47:42 +0000

    Add functionality to add and remove expenses on firebase

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import {editExpense, removeExpense} from '../actions/expenses';
+import {editExpense, startRemoveExpense} from '../actions/expenses';
 
 export class EditExpensePage extends React.Component
 {
@@ -10,8 +10,8 @@ export class EditExpensePage extends React.Component
     this.props.history.push('/');
   };
 
-  removeExpense = () => {
-    this.props.removeExpense({id: this.props.expense.id});
+  startRemoveExpense = () => {
+    this.props.startRemoveExpense({id: this.props.expense.id});
     this.props.history.push('/');
   }
 
@@ -23,7 +23,7 @@ export class EditExpensePage extends React.Component
           onSubmit={this.editExpense}
         />
         <button
-          onClick={this.removeExpense}
+          onClick={this.startRemoveExpense}
         >
           Remove
         </button>
@@ -34,7 +34,7 @@ export class EditExpensePage extends React.Component
 
 const mapDispatchToProps = (dispatch) => ({
   editExpense: (id, expense) => dispatch(editExpense(id, expense)),
-  removeExpense: (data) => dispatch(removeExpense(data))
+  startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
 });
 
 const mapStateToProps = (state, props) => {
--- expensify-app/src/tests/components/EditExpensePage.test.js
+++ expensify-app/src/tests/components/EditExpensePage.test.js
@@ -3,18 +3,18 @@ import {shallow} from 'enzyme';
 import {EditExpensePage} from '../../components/EditExpensePage';
 import expenses from '../fixtures/expenses';
 
-let editExpense, removeExpense, history, wrapper;
+let editExpense, startRemoveExpense, history, wrapper;
 
 beforeEach(() => {
   editExpense = jest.fn();
-  removeExpense = jest.fn();
+  startRemoveExpense = jest.fn();
   history = {push: jest.fn()};
   wrapper = shallow(
     <EditExpensePage
       history={history}
       expense={expenses[2]}
       editExpense={editExpense}
-      removeExpense={removeExpense}
+      startRemoveExpense={startRemoveExpense}
     />
   );
 });
@@ -33,5 +33,5 @@ test('should handle editExpense', () => {
 test('should handle removeExpense', () => {
   wrapper.find('button').simulate('click');
   expect(history.push).toHaveBeenLastCalledWith('/');
-  expect(removeExpense).toHaveBeenLastCalledWith({id:expenses[2].id});
+  expect(startRemoveExpense).toHaveBeenLastCalledWith({id:expenses[2].id});
 });
